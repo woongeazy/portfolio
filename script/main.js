8afa45e81e6f2e3780cc5432ea89c44df8d7ad99
hanchaewoong <hco2995@naver.com>
2023-11-13 09:54:05 +0900

    탭 진행중

--- script/main.js
+++ script/main.js
@@ -1,3 +1,34 @@
-(function(){
+$(document).ready(function() {
+    $('a').on("click", function(e) {
+        e.preventDefault();
+    });
+});
+// a 기능 비활성화
+
+$(function(){
+    $('.design_title > li:nth-last-child(2)').on('click', function(){
+        $('.design_contents > ul').stop().slideDown(300);
+        $('.design_contents > h5').removeClass('d-none');
+        $('.design_title > li:nth-last-child(1)').removeClass('d-none')
+        $(this).addClass('d-none');
+    });
+    $('.design_title > li:nth-last-child(1)').on('click', function(){
+        $('.design_contents > ul').stop().siblings().slideUp(300);
+        $('.design_contents > h5').addClass('d-none');
+        $('.design_title > li:nth-last-child(2)').removeClass('d-none')
+        $(this).addClass('d-none')
+    });
+
+
+
+    let tabBtn = $('.design_title > li');
+    let tabCont = $('.design_contents > ul');
+    tabCont.hide().eq(0).show();
     
-})
+    
+    $(tabBtn).on('click', function(){
+        const index = $(this).index();
+        tabCont.eq(index).show().siblings().hide();
+
+    })
+});
