a0c1722b52e2df2890ff0822b9d9565d5e4146e8
hanchaewoong <hco2995@naver.com>
2023-11-24 21:26:00 +0900

    sass 추가 및 header 레이아웃 완성

--- script/main.js
+++ script/main.js
@@ -1,34 +1,33 @@
-$(document).ready(function() {
-    $('a').on("click", function(e) {
-        e.preventDefault();
-    });
+$(document).ready(function () {
+  $(".tab_title > a").on("click", function (e) {
+    e.preventDefault();
+  });
 });
 // a 기능 비활성화
 
-$(function(){
-    $('.design_title > li:nth-last-child(2)').on('click', function(){
-        $('.design_contents > ul').stop().slideDown(300);
-        $('.design_contents > h5').removeClass('d-none');
-        $('.design_title > li:nth-last-child(1)').removeClass('d-none')
-        $(this).addClass('d-none');
-    });
-    $('.design_title > li:nth-last-child(1)').on('click', function(){
-        $('.design_contents > ul').stop().siblings().slideUp(300);
-        $('.design_contents > h5').addClass('d-none');
-        $('.design_title > li:nth-last-child(2)').removeClass('d-none')
-        $(this).addClass('d-none')
-    });
+$(function () {
+  // tabCont.hide().eq(0).show(); 
 
+  $(tabBtn).on("click", function () {
+    const index = $(this).index();
+    tabCont.eq(index).show().siblings().hide();
+  });
 
-
-    let tabBtn = $('.design_title > li');
-    let tabCont = $('.design_contents > ul');
-    tabCont.hide().eq(0).show();
-    
-    
-    $(tabBtn).on('click', function(){
-        const index = $(this).index();
-        tabCont.eq(index).show().siblings().hide();
-
+  $('.main_content ul > li').on('mouseover', function(){
+    $(this).find('img').css({
+        width: '120%'
     })
+  })
+  $('.main_content ul > li').on('mouseout', function(){
+    $(this).find('img').css({
+        width: '100%'
+    })
+  })
+
+
+
+
 });
+
+let tabBtn = $(".design_title > li");
+let tabCont = $(".design_contents > .main_content");
